1d1992dbd799abd96d354bed448524e89365ac37
David Dimon <david_dimon@hotmail.com>
2020-06-14 18:15:35 -0300

    fix: publish script

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,20 @@
+const proj = require('./package.json').name;
+
 module.exports = {
+  pathPrefix: `/${proj}`,
   plugins: [
     {
       resolve: 'gatsby-theme-mdx-deck',
       options: {
-        // enable or disable gatsby-plugin-mdx
-        mdx: false,
-        // source directory
-        contentPath: 'src',
-        // base path for routes generate by this theme
-        basePath: ''
-      }
-    }
-  ]
-}
+        cli: true,
+        contentPath: 'slides',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-compile-es6-packages',
+      options: {
+        modules: ['mdx-deck', 'gatsby-theme-mdx-deck', '@mdx-deck/themes', '@mdx-deck/gatsby-plugin'],
+      },
+    },
+  ],
+};
